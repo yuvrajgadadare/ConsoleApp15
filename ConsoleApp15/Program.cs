a1f12b00d257e00553aa0309cb6c2e3d0cbc03fa
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2025-07-28 09:00:02 +0530

    Changes

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -2,6 +2,11 @@
 {
     public class Program
     {
+
+        void Add()
+        {
+            int a, b, c;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Core App");
